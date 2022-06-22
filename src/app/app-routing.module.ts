f659df27d49a84815df81d654efc9019b309f704
iroussenq <igorroussenq273@outlook.com>
2022-06-22 15:23:48 -0300

    mais feitos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,7 @@
+import { ProdutoComponent } from './produto/produto.component';
+import { FornecedorComponent } from './fornecedor/fornecedor.component';
+import { FarmaceuticoComponent } from './farmaceutico/farmaceutico.component';
+import { ClienteComponent } from './cliente/cliente.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
@@ -6,6 +10,10 @@ import { SobreComponent } from './sobre/sobre.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'sobre', component: SobreComponent },
+  { path: 'cliente', component: ClienteComponent },
+  { path: 'farmaceutico', component: FarmaceuticoComponent },
+  { path: 'fornecedor', component: FornecedorComponent },
+  { path: 'produto', component: ProdutoComponent },
 ];
 
 @NgModule({
